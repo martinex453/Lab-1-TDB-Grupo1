776ea3827d1f5954a62533e45b2b97992c0ac6db
Alvaro Z <alvaro.zamorano@usach.cl>
2024-11-04 21:15:06 -0300

    Constructor, Get y Set
    De las clases Categoria, Cliente y Detalle orden.

--- back-grupo1/src/main/java/BDA/grupo1/model/Categoria.java
+++ back-grupo1/src/main/java/BDA/grupo1/model/Categoria.java
@@ -6,4 +6,25 @@ import lombok.Data;
 public class Categoria {
     private Long id_categoria;
     private String nombre;
+
+    public Categoria(String nombre, Long id_categoria) {
+        this.nombre = nombre;
+        this.id_categoria = id_categoria;
+    }
+
+    public Long getId_categoria() {
+        return id_categoria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setId_categoria(Long id_categoria) {
+        this.id_categoria = id_categoria;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
--- back-grupo1/src/main/java/BDA/grupo1/model/Cliente.java
+++ back-grupo1/src/main/java/BDA/grupo1/model/Cliente.java
@@ -9,4 +9,62 @@ public class Cliente {
     private String direccion;
     private String email;
     private String telefono;
+    private String contraseña;
+
+    public Cliente(Long id_cliente, String nombre, String direccion, String email, String telefono, String contraseña) {
+        this.id_cliente = id_cliente;
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.email = email;
+        this.telefono = telefono;
+        this.contraseña = contraseña;
+    }
+
+    public Long getId_cliente() {
+        return id_cliente;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setId_cliente(Long id_cliente) {
+        this.id_cliente = id_cliente;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
 }
